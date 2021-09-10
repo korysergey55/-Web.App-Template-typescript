3462e40bd56fa62cd4318265f9c2085e9ac251fc
Serhiy Korytskyy <korysergey55@gmail.com>
2021-09-10 18:03:47 +0300

    Updates

---
+++ precache-manifest.b1497679175ac6ef78935ca181f4dcf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105a6c62a56b9930fc7a85690b0728b8",
+    "url": "/Web.App-Template-typescript/index.html"
+  },
+  {
+    "revision": "64d899b1f486773d98c1",
+    "url": "/Web.App-Template-typescript/static/css/2.5cc265e8.chunk.css"
+  },
+  {
+    "revision": "5246ebf00c5be0129ec3",
+    "url": "/Web.App-Template-typescript/static/css/main.f82a190a.chunk.css"
+  },
+  {
+    "revision": "64d899b1f486773d98c1",
+    "url": "/Web.App-Template-typescript/static/js/2.f4f0119e.chunk.js"
+  },
+  {
+    "revision": "dda5b236ecc8cb7d2ff0c1f24bdef29f",
+    "url": "/Web.App-Template-typescript/static/js/2.f4f0119e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5246ebf00c5be0129ec3",
+    "url": "/Web.App-Template-typescript/static/js/main.fcc53d3c.chunk.js"
+  },
+  {
+    "revision": "7c713316aec4372c0c1d",
+    "url": "/Web.App-Template-typescript/static/js/runtime-main.26c014b8.js"
+  }
+]);
